754e9a447b749a655b683ed10a24afc914fd26de
janvt <janvt@me.com>
2014-02-12 20:33:11 +0100

    readConfFile

--- functions.php
+++ functions.php
@@ -12,7 +12,22 @@ function getProperty($name)
 
 function readConfFile($path)
 {
+    if(!file_exists($path))
+        throw new Exception('ERROR reading config file! ' . $path);
 
+    $fc = file_get_contents($path);
+    $lines = explode("\n", $fc);
+
+    if(count($lines) === 0)
+        throw new Exception('ERROR! empty conf file!' . $path);
+
+    $r = array();
+    foreach($lines as $line)
+    {
+        $r[] = str_getcsv($line);
+    }
+
+    return $r;
 }
 
 ?>
