37ebfe24ea9dae50553a5ac1f25e60ad79c2a537
janvt <janvt@me.com>
2014-02-16 18:08:48 +0100

    first models / views

--- js/hund.js
+++ js/hund.js
@@ -1,3 +1,49 @@
-;(function(global, $, _, Backbone) {
+;(function(global, $, _, Backbone)
+{
+    /*** models ***/
+    var ImageModel = Backbone.Model.extend({
+        defaults: {
+            url: ''
+            , thumbUrl: ''
+            , category: ''
+            , title: ''
+            , description: ''
+        }
+    });
     
-}(this, jQuery, _, Backbone));
+    /*** views ***/
+    var MainView = Backbone.View.extend({
+        events: {
+            'click .tile': 'onClick'
+        }
+        
+        , initialize: function(options)
+        {
+            this.options = _.extend({
+                el: '#'
+            }, options);
+            
+            this.listenTo(this.collection)
+        }
+    });
+    
+    /*** ***/
+    _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
+    var tileTemplate = _.template('<a class="tile" href="{{ href }}"><div>{{ content }}</div></a>')
+        , images = new (Backbone.Collection.extend({ model: ImageModel }))
+    ;
+    
+    function init()
+    {
+        
+        var data = global.lovelyData;
+        console.debug('init!', data);
+        if(data.images)
+        {
+            images.add(data.images);
+        }
+    }
+    
+    // jvt: run init on document ready
+    $(init);
+}(window, jQuery, _, Backbone));
